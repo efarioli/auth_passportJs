33007332c1453d2cfb4fc0b861ec56380c574a19
zeke farioli <efarioli@gmail.com>
2017-04-02 17:24:51 +0100

    add Register routes - add Register form

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(require("express-session")({
 	secret: "degreasing engines and killing brain cells",
@@ -26,12 +27,38 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//================================
+//ROUTES
+//================================
+
+
 app.get("/", function(req,res){
 	res.render("home");
 });
 
 app.get("/secret", function(req, res){
 	res.render("secret");
+
+});
+
+//Auth Routes
+
+//show sign up form
+app.get("/register", function(req,res){
+	res.render("register");
+});
+
+//handling user singup
+app.post("/register", function(req,res){
+	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req,res, function(){
+			res.redirect("/secret");
+		});
+	});
 
 });
 
