f3ba60588be722e784cf66221c3010f8c9054fae
zeke farioli <efarioli@gmail.com>
2017-04-02 16:58:03 +0100

    Create User model and configure passportjs

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express               =                 require("express"),
+    mongoose              =                require("mongoose"),
+    passport              =                require("passport"),
+    bodyParser            =             require("body-parser"),
+    User                  =           require("./models/user"),
+    LocalStrategy         =          require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
 mongoose.connect("mongodb://localhost/auth_demo_app");
+
+
 
 
 var app = express();
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+	secret: "degreasing engines and killing brain cells",
+	resave: false,
+	saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
 	res.render("home");
