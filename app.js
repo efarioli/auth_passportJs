e35af81c6c7483f79ea3ac77ace4f7f055f86b96
zeke farioli <efarioli@gmail.com>
2017-04-03 10:14:32 +0100

    add Logout route - add isLoggedIn middleware

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get("/", function(req,res){
 	res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
 	res.render("secret");
 
 });
@@ -74,6 +74,19 @@ app.post("/login", passport.authenticate("local", {
 	failureRedirect: "/login"
 }) , function(req,res){
 });
+
+//logout
+app.get("/logout", function(req,res){
+	req.logout();
+	res.redirect("/");
+});
+
+function isLoggedIn(req,res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("login");
+}
 
 app.listen("3000", "localhost", function(){
 	console.log("Server started in Localhost port 3000");
